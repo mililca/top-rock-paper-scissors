2fc68bbe66a113fffe36f1ac3b9655796166bd68
mililca <141839434+mililca@users.noreply.github.com>
2023-08-31 15:08:10 +0200

    Add function to check player's choice

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const choices = ["Rock", "Paper", "Scissors"]
+const choices = ["rock", "paper", "scissors"]
 
 function getComputerChoice () {
     let computerChoice = choices[Math.floor((Math.random() * choices.length))]
@@ -8,3 +8,16 @@ function getComputerChoice () {
 let computerChoice = getComputerChoice()
 
 // console.log(computerChoice) //
+
+let playerChoice = prompt("Make your choice: ")
+let lowerPlayerChoice = playerChoice.toLowerCase()
+
+function checkPlayerChoice (choice) {
+    if (!(choices.includes(lowerPlayerChoice))) {
+        console.log("Pick one from rock, paper or scissors.")
+    }
+}
+
+checkPlayerChoice(lowerPlayerChoice)
+
+ // console.log(playerChoice) //
